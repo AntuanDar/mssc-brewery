403486b37b6c8915f05bf57d491e21d72de33cf7
Anton <antuan_dar@bk.ru>
2021-08-02 23:20:06 +0300

    error handler + validation

--- src/main/java/com/taranenkonat/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/com/taranenkonat/msscbrewery/web/controller/CustomerController.java
@@ -6,10 +6,17 @@ import com.taranenkonat.msscbrewery.web.model.CustomerDto;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+@Validated
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
@@ -21,12 +28,12 @@ public class CustomerController {
     }
 
     @GetMapping({"/{customerId}"})
-    public ResponseEntity<CustomerDto> getBeer(@PathVariable("customerId") UUID customerId){
+    public ResponseEntity<CustomerDto> getBeer(@NotNull @PathVariable("customerId") UUID customerId){
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity handlePost(@RequestBody CustomerDto customerDto){
+    public ResponseEntity handlePost(@Valid @RequestBody CustomerDto customerDto){
         CustomerDto savedCustomer = customerService.saveNewCustomer(customerDto);
         HttpHeaders httpHeaders = new HttpHeaders();
         //todo add hostname to url
@@ -36,7 +43,7 @@ public class CustomerController {
     }
 
     @PutMapping("/{customerId}")
-    public ResponseEntity handleUpdate(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto customerDto){
+    public ResponseEntity handleUpdate(@PathVariable("customerId") UUID customerId, @Valid @RequestBody CustomerDto customerDto){
         customerService.updateCustomer(customerId, customerDto);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
